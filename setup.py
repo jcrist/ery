95c23c2f148135b043239cc5aa625ff2129c88ad
Jim Crist-Harif <jcristharif@gmail.com>
2020-05-16 12:46:03 -0500

    Initial project structure

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+
+from setuptools import setup
+
+setup(
+    name="ery",
+    version="0.0.1",
+    maintainer="Jim Crist-Harif",
+    maintainer_email="jcristharif@gmail.com",
+    license="BSD",
+    packages=["ery"],
+    install_requires=["msgpack"],
+    long_description=(
+        open("README.rst").read() if os.path.exists("README.rst") else ""
+    ),
+    zip_safe=False,
+)
