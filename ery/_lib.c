9717e92c8ae1aa90ce07512ce50347942cf5edf0
Jim Crist-Harif <jcristharif@gmail.com>
2020-06-08 18:23:15 -0500

    C-api

---
+++ ery/_lib.c
@@ -0,0 +1,322 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+#include <stdbool.h>
+#include <string.h>
+
+enum Op {
+    OP_KIND = 0,
+    OP_FLAGS = 1,
+    OP_HEARTBEAT = 2,
+    OP_ID = 3,
+    OP_CODE = 4,
+    OP_WINDOW = 5,
+    OP_ROUTE_LENGTH = 6,
+    OP_METADATA_LENGTH = 7,
+    OP_BODY_LENGTH = 8,
+    OP_NFRAMES = 9,
+    OP_FRAME_LENGTHS = 10,
+    OP_ROUTE = 11,
+    OP_METADATA = 12,
+    OP_BODY = 13,
+    OP_FRAMES = 14,
+};
+
+enum Kind {
+    KIND_UNKNOWN = 0,
+    KIND_SETUP = 1,
+    KIND_SETUP_RESPONSE = 2,
+    KIND_HEARTBEAT = 3,
+    KIND_ERROR = 4,
+    KIND_CANCEL = 5,
+    KIND_INCREMENT_WINDOW = 6,
+    KIND_REQUEST = 7,
+    KIND_NOTICE = 8,
+    KIND_REQUEST_STREAM = 9,
+    KIND_REQUEST_CHANNEL = 10,
+    KIND_PAYLOAD = 11,
+};
+
+typedef struct {
+    PyObject_HEAD
+    // -- static attributes --
+    PyObject *msg_callback;
+    // default buffer
+    char *default_buffer;
+    Py_ssize_t default_buffer_size;
+    Py_ssize_t default_buffer_start;
+    Py_ssize_t default_buffer_end;
+    // default_frame_lengths
+    unsigned int *default_frame_lengths_buffer;
+    unsigned int default_frame_lengths_buffer_size;
+    // -- dynamic attributes --
+    enum Kind kind;
+    enum Op op;
+    char flags;
+    unsigned int id;
+    // route
+    unsigned int route_length;
+    unsigned int route_index;
+    PyObject *route;
+    // metadata
+    unsigned int metadata_length;
+    unsigned int metadata_index;
+    PyObject *metadata;
+    // nframes
+    unsigned short nframes;
+    // frame lengths
+    unsigned int *frame_lengths;
+    unsigned short frame_lengths_index;
+    // frames
+    PyObject *frames;
+    Py_ssize_t frame_index;
+    // frame buffer
+    PyObject *frame_buffer;
+    Py_ssize_t frame_buffer_index;
+    bool using_frame_buffer;
+} ProtocolObject;
+
+static PyObject *
+Protocol_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
+{
+    ProtocolObject *self;
+    self = (ProtocolObject *) type->tp_alloc(type, 0);
+    if (self == NULL) {
+        return NULL;
+    }
+    return (PyObject *) self;
+}
+
+static void
+Protocol_reset_message(ProtocolObject *self)
+{
+    self->op = OP_KIND;
+    self->kind = KIND_UNKNOWN;
+    self->flags = 0;
+    self->id = 0;
+    self->route_length = 0;
+    // NOTE: this should already be NULL when called
+    self->route = NULL;
+    self->route_index = 0;
+    self->metadata_length = 0;
+    self->metadata = NULL;
+    self->metadata_index = 0;
+    self->nframes = 0;
+    // TODO: when dynamc, will need to free here
+    self->frame_lengths = NULL;
+    self->frame_lengths_index = 0;
+    self->frames = NULL;
+    self->frame_index = 0;
+    self->frame_buffer = NULL;
+    self->frame_buffer_index = 0;
+}
+
+static int
+Protocol_init(ProtocolObject *self, PyObject *args, PyObject *kwds)
+{
+    static char *kwlist[] = {"msg_callback", "buffer_size", "frame_lengths_size", NULL};
+    Py_ssize_t buffer_size = 256 * 1024;
+    Py_ssize_t frame_lengths_size = 32;
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwds, "O|nn", kwlist, &self->msg_callback,
+                                     &buffer_size, &frame_lengths_size)) {
+        return -1;
+    }
+    if (buffer_size < 4) {
+        PyErr_SetString(PyExc_ValueError, "buffer_size must be >= 4");
+        return -1;
+    }
+    if (frame_lengths_size < 1) {
+        PyErr_SetString(PyExc_ValueError, "buffer_size must be >= 1");
+        return -1;
+    }
+    Py_INCREF(self->msg_callback);
+    /* default buffer management */
+    self->default_buffer = (char *)PyMem_Malloc(buffer_size);
+    if (self->default_buffer == NULL) {
+        PyErr_SetNone(PyExc_MemoryError);
+        return -1;
+    }
+    self->default_buffer_size = buffer_size;
+    self->default_buffer_start = 0;
+    self->default_buffer_end = 0;
+    /* frame lengths buffer management */
+    self->default_frame_lengths_buffer_size = frame_lengths_size;
+    self->default_frame_lengths_buffer = (unsigned int *)PyMem_Malloc(frame_lengths_size * sizeof(unsigned int));
+    if (self->default_frame_lengths_buffer == NULL) {
+        PyErr_SetNone(PyExc_MemoryError);
+        return -1;
+    }
+    /* dynamic state */
+    self->using_frame_buffer = false;
+    Protocol_reset_message(self);
+    return 0;
+}
+
+static int
+Protocol_clear(ProtocolObject *self)
+{
+    Py_CLEAR(self->msg_callback);
+    return 0;
+}
+
+static int
+Protocol_traverse(ProtocolObject *self, visitproc visit, void *arg)
+{
+    return visit(self->msg_callback, arg);
+}
+
+static void
+Protocol_dealloc(ProtocolObject *self)
+{
+    PyObject_GC_UnTrack(self);
+    Protocol_clear(self);
+    if (self->default_buffer != NULL) {
+        PyMem_Free(self->default_buffer);
+        self->default_buffer = NULL;
+    }
+    if (self->default_frame_lengths_buffer != NULL) {
+        PyMem_Free(self->default_frame_lengths_buffer);
+        self->default_frame_lengths_buffer = NULL;
+    }
+    Py_TYPE(self)->tp_free((PyObject *) self);
+}
+
+static PyObject*
+Protocol_get_buffer(ProtocolObject *self)
+{
+    if (self->op < OP_FRAMES) {
+        self->using_frame_buffer = false;
+        return PyMemoryView_FromMemory(
+            self->default_buffer + self->default_buffer_end,
+            self->default_buffer_size - self->default_buffer_end,
+            PyBUF_WRITE
+        );
+    } else {
+        Py_ssize_t frame_buffer_size = self->frame_lengths[self->frame_lengths_index];
+        if (self->frame_buffer == NULL) {
+            self->frame_buffer = PyByteArray_FromStringAndSize(NULL, frame_buffer_size);
+            if (self->frame_buffer == NULL) {
+                return NULL;
+            }
+            self->frame_buffer_index = 0;
+        }
+        Py_ssize_t to_read = frame_buffer_size - self->frame_buffer_index;
+        if (to_read >= self->default_buffer_size) {
+            self->using_frame_buffer = true;
+            return PyMemoryView_FromMemory(
+                PyByteArray_AS_STRING(self->frame_buffer) + self->frame_buffer_index,
+                frame_buffer_size - self->frame_buffer_index,
+                PyBUF_WRITE
+            );
+        } else {
+            self->using_frame_buffer = false;
+            return PyMemoryView_FromMemory(
+                self->default_buffer + self->default_buffer_end,
+                self->default_buffer_size - self->default_buffer_end,
+                PyBUF_WRITE
+            );
+        }
+    }
+}
+
+static void
+Protocol_reset_default_buffer(ProtocolObject *self) {
+    Py_ssize_t start = self->default_buffer_start;
+    Py_ssize_t end = self->default_buffer_end;
+
+    if (start == 0) {
+        return;
+    } else if (start < end) {
+        memmove(self->default_buffer, self->default_buffer + start, end - start);
+        self->default_buffer_start = 0;
+        self->default_buffer_end = end - start;
+    } else {
+        self->default_buffer_start = 0;
+        self->default_buffer_end = 0;
+    }
+}
+
+static int
+Protocol_advance(ProtocolObject *self) {
+    // TODO
+    return 1;
+}
+
+static PyObject*
+Protocol_buffer_updated(ProtocolObject *self, PyObject *args)
+{
+    Py_ssize_t nbytes = 0;
+    if (!PyArg_ParseTuple(args, "n", &nbytes)) {
+        return NULL;
+    }
+
+    if (nbytes == 0) {
+        Py_RETURN_NONE;
+    }
+
+    if (self->using_frame_buffer) {
+        self->frame_buffer_index += nbytes;
+    } else {
+        self->default_buffer_end += nbytes;
+    }
+
+    int status = 0;
+    while (status == 0) {
+        status = Protocol_advance(self);
+    }
+    if (status < 0) {
+        return NULL;
+    }
+    Protocol_reset_default_buffer(self);
+    Py_RETURN_NONE;
+}
+
+
+static PyMethodDef Protocol_methods[] = {
+    {"get_buffer", (PyCFunction) Protocol_get_buffer, METH_NOARGS, PyDoc_STR("get_buffer() -> memoryview")},
+    {"buffer_updated", (PyCFunction) Protocol_buffer_updated, METH_VARARGS, PyDoc_STR("buffer_updated(nbytes: int) -> None")},
+    {NULL},
+};
+
+static PyTypeObject ProtocolType = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    .tp_name = "ery._lib.Protocol",
+    .tp_doc = "A sans-io protocol for ery",
+    .tp_basicsize = sizeof(ProtocolObject),
+    .tp_itemsize = 0,
+    .tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_GC,
+    .tp_new = Protocol_new,
+    .tp_init = (initproc) Protocol_init,
+    .tp_dealloc = (destructor) Protocol_dealloc,
+    .tp_clear = (inquiry) Protocol_clear,
+    .tp_traverse = (traverseproc) Protocol_traverse,
+    .tp_methods = Protocol_methods,
+};
+
+static PyModuleDef erylibmodule = {
+    PyModuleDef_HEAD_INIT,
+    .m_name = "ery._lib",
+    .m_doc = "c-extension core for ery",
+    .m_size = -1,
+};
+
+PyMODINIT_FUNC
+PyInit__lib(void)
+{
+    PyObject *m;
+    if (PyType_Ready(&ProtocolType) < 0)
+        return NULL;
+
+    m = PyModule_Create(&erylibmodule);
+    if (m == NULL)
+        return NULL;
+
+    Py_INCREF(&ProtocolType);
+    if (PyModule_AddObject(m, "Protocol", (PyObject *) &ProtocolType) < 0) {
+        Py_DECREF(&ProtocolType);
+        Py_DECREF(m);
+        return NULL;
+    }
+
+    return m;
+}
--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import os
 
 from setuptools import setup
+from setuptools.extension import Extension
+
+ext_modules = [Extension("ery._lib", ["ery/_lib.c"])]
 
 setup(
     name="ery",
@@ -9,6 +12,7 @@ setup(
     maintainer_email="jcristharif@gmail.com",
     license="BSD",
     packages=["ery"],
+    ext_modules=ext_modules,
     long_description=(
         open("README.rst").read() if os.path.exists("README.rst") else ""
     ),
